e8c976771109037ba37bd77247b246e1b62288f4
YehoLav123 <yehonatanlavy2008v2@gmail.com>
2024-12-28 21:42:59 +0200

    Add project files.

---
+++ XTry/Registration.cs
@@ -0,0 +1,14 @@
+﻿namespace XTry
+{
+    public class Registration
+    {
+        private string username;
+        private string email;
+        private string password;
+
+        public string Username { get => username; set => username = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+
+    }
+}
